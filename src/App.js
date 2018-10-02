8f71d21f7fde0b2e5f7f418126e055367caecbe6
Pushkar Shembekar <pushembekar@gmail.com>
2018-10-01 20:40:12 -0400

    Working with lists

--- src/App.js
+++ src/App.js
@@ -13,16 +13,12 @@ class App extends Component {
     showPersons : false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!')
-    // DON'T DO THIS this.state.persons[0].name = 'Maximilian';
-    this.setState({persons : [
-      { name: newName, age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 27 }
-    ]})
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
-
+ 
   nameChangedHandler = (event) => {
     this.setState({persons : [
       { name: 'Max', age: 28 },
@@ -43,30 +39,29 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+            {this.state.persons.map((person, index) => {
+              return <Person name={person.name} age={person.age} click={() => this.deletePersonHandler(index)} />
+            })}
+        </div>
+      );
     }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
         <button style={style} 
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { 
-          this.state.showPersons ? 
-          <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age} />
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age} />
-          </div>
-          : null 
-        }
+
+        {persons}          
+         
       </div>
     );
     //return React.createElement('div', null, React.createElement('h1', {className: 'App'}, "Does this work now?"));
