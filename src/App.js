e5bfdfbfaae245f83b5142dceaf2ab0f9cc88262
Pushkar Shembekar <pushembekar@gmail.com>
2018-10-02 20:40:59 -0400

    Using radium css styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -87,15 +87,17 @@ class App extends Component {
 
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working</p>
-        <button style={style} 
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working</p>
+          <button style={style} 
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
-        {persons}          
-         
-      </div>
+          {persons}          
+          
+        </div>
+      </StyleRoot>
     );
     //return React.createElement('div', null, React.createElement('h1', {className: 'App'}, "Does this work now?"));
   }
