f91d69e4739f84f2efac3581f6ddfd408b90e050
Pushkar Shembekar <pushembekar@gmail.com>
2018-10-02 23:09:12 -0400

    Modifying webpack to allow css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -42,16 +42,9 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
+    
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -63,26 +56,25 @@ class App extends Component {
             })}
         </div>
       );
-
-      style.backgroundColor = 'red';
+      
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
 
     if(this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if(this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working</p>
-          <button style={style} 
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          <p className={assignedClasses.join(' ')}>This is really working</p>
+          <button onClick={this.togglePersonsHandler} className={btnClass}>Toggle Persons</button>
 
           {persons}          
           
